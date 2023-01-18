fbd1ce472379c8fa8243d0e2438c560e14a9e941
AndreaChiari <chiariandrea94@gmail.com>
2023-01-18 14:20:04 +0100

    fine-sesercizio

--- mail-list.js
+++ mail-list.js
@@ -1,1 +1,39 @@
 console.log('vue ok',Vue)
+
+
+const app = Vue.createApp({
+    name:'Mail list',
+    data() {
+        return{
+            nameMail: []
+        }
+    },
+    methods:{
+        getRandomMail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.nameMail.push( response.data.response);
+            });
+
+        }
+    },
+
+    mounted(){
+        for (let i = 0; i < 10; i++){
+            this.getRandomMail();
+        }
+     },
+  
+    });
+    
+    app.mount('#root')
+            
+
+           
+       
+    
+    
+   
+
+ 
+           
